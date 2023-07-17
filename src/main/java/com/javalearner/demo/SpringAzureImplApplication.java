00bd19216b5ed42164f0498e75ecb3f8936c311a
submohan4 <subhammohanty44@gmail.com>
2023-07-17 13:21:59 +0530

    first commit

---
+++ src/main/java/com/javalearner/demo/SpringAzureImplApplication.java
@@ -0,0 +1,13 @@
+package com.javalearner.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAzureImplApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAzureImplApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javalearner/demo/SpringAzureImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javalearner.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureImplApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
